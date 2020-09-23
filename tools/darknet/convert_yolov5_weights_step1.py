4461e58685fa7e8e976ebd22774bd39093fc184b
pi <pi@pi.com>
2020-09-23 14:02:52 +0800

    修改转换脚本

--- tools/darknet/convert_yolov5_weights_step1.py
+++ tools/darknet/convert_yolov5_weights_step1.py
@@ -1,11 +1,25 @@
 import torch
+import os
+from tools.darknet.yolov5_download import attempt_download
 
 
-# 该程序必须用pytorch1.6的版本才能读取
+# 注意本程序无法运行！！！
+# 该程序必须放在yolov5工程目录下，用pytorch1.6的版本才能读取
 
 if __name__ == '__main__':
-    weights_file = '/home/pi/pytorch_2020/yolov5/yolov5s.pt'
-    weight = torch.load(weights_file, map_location='cpu')['model']  # 只能用pytorch1.6才能加载
+    # v3版本下载地址 https://github.com/ultralytics/yolov5/releases/tag/v3.0
+
+    is_download = False
+    save_name = 'yolov5s.pth'
+
+    if is_download:
+        name = 'yolov5s.pt'
+        attempt_download(name)
+        weights_file = name
+    else:
+        weights_file = '/home/pi/yolov5s.pt'
+
+    weight = torch.load(weights_file, map_location=torch.device('cpu'))['model']  # 只能用pytorch1.6才能加载
     print('cfg', weight.yaml)
     print('stride', weight.stride)
     print('hyp', weight.hyp)
@@ -15,7 +29,6 @@ if __name__ == '__main__':
     model = weight.model
     state_dict = model.state_dict()
     data = {"state_dict": state_dict}
-    print(state_dict.keys())
+    # print(state_dict.keys())
     # pytorch1.6的保存格式更改了，低版本无法读取，需要采用_use_new_zipfile_serialization
-    torch.save(data, '../../yolov5s.pth', _use_new_zipfile_serialization=False)
-
+    torch.save(data, save_name, _use_new_zipfile_serialization=False)
--- tools/darknet/convert_yolov5_weights_step2.py
+++ tools/darknet/convert_yolov5_weights_step2.py
@@ -4,19 +4,23 @@ from collections import OrderedDict
 # 该程序必须用mmdetection环境读取
 
 if __name__ == '__main__':
-    weights_file = '../../yolov5s.pth'
-    data = torch.load('../../yolov5s.pth')['state_dict']
-    # print(data.keys())
 
-    # 不同的模型 10和24需要改
+    name = 'yolov5s'  # yolov5s yolov5m yolov5l yolov5x
+
+    weights_file = '/home/pi/pytorch_2020/yolov5/yolov5s.pth'
+    save_name = '../../yolov5s_mm.pth'
+
+    data = torch.load(weights_file)['state_dict']
+
+    index = [10, 24]
 
     new_state_dict = OrderedDict()
     for k, v in data.items():
         num = int(k.split('.')[0])
-        if num < 10:  # 前9个是骨架
+        if num < index[0]:  # 前9个是骨架
             name = 'backbone.backbone.' + k
-        elif num < 24:  # neck
-            name = 'bbox_head.det.' + str(num - 10) + k[2:]
+        elif num < index[1]:  # neck
+            name = 'bbox_head.det.' + str(num - index[0]) + k[2:]
         else:  # head
             name = 'bbox_head.head.' + k[5:]
 
@@ -25,6 +29,6 @@ if __name__ == '__main__':
 
         new_state_dict[name] = v
 
-    print(new_state_dict.keys())
+    # print(new_state_dict.keys())
     data = {"state_dict": new_state_dict}
-    torch.save(data, '../../yolo5s_mm.pth')
+    torch.save(data, save_name)
