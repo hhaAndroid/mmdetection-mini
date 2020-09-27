dad8367880a4e321b8ac64ee95d712da44d232d9
hha <1286304229@qq.com>
2020-09-27 08:07:29 +0800

    修复导入bug

--- mmdet/cv_core/parallel/registry.py
+++ mmdet/cv_core/parallel/registry.py
@@ -1,6 +1,6 @@
 from torch.nn.parallel import DataParallel
 
-from mmcv.utils import Registry
+from ..utils import Registry
 
 MODULE_WRAPPERS = Registry('module wrapper')
 MODULE_WRAPPERS.register_module(module=DataParallel)
