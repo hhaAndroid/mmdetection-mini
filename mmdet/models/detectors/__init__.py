ce26c423a76b5785f50f66f95eb7ad0de21d6601
hha <1286304229@qq.com>
2020-10-24 10:31:24 +0800

    新增cascade rcnn代码

--- mmdet/models/detectors/__init__.py
+++ mmdet/models/detectors/__init__.py
@@ -1,7 +1,8 @@
 from .base import BaseDetector
 from .single_stage import SingleStageDetector
 from .two_stage import TwoStageDetector
+from .cascade_rcnn import CascadeRCNN
 
 __all__ = [
-    'BaseDetector', 'SingleStageDetector', 'TwoStageDetector'
+    'BaseDetector', 'SingleStageDetector', 'TwoStageDetector', 'CascadeRCNN'
 ]
--- mmdet/models/roi_heads/__init__.py
+++ mmdet/models/roi_heads/__init__.py
@@ -2,7 +2,8 @@ from .base_roi_head import BaseRoIHead
 from .bbox_heads import (BBoxHead)
 from .roi_extractors import SingleRoIExtractor
 from .standard_roi_head import StandardRoIHead
+from .cascade_roi_head import CascadeRoIHead
 
 __all__ = [
-    'BaseRoIHead', 'BBoxHead', 'StandardRoIHead'
+    'BaseRoIHead', 'BBoxHead', 'StandardRoIHead', 'CascadeRoIHead'
 ]
