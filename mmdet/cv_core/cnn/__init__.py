bc3b77263f3babd8287ce6ec23a3e17fcbc86c7e
hha <1286304229@qq.com>
2020-10-25 09:30:28 +0800

    新增non local模块

--- mmdet/cv_core/cnn/__init__.py
+++ mmdet/cv_core/cnn/__init__.py
@@ -5,9 +5,10 @@ from .bricks import (ACTIVATION_LAYERS, CONV_LAYERS, NORM_LAYERS,
                      ConvModule, build_activation_layer,
                      build_conv_layer,
                      build_norm_layer, build_padding_layer,
-                     build_upsample_layer, is_norm,Scale)
+                     build_upsample_layer, is_norm, Scale,
+                     NonLocal1d, NonLocal2d, NonLocal3d)
 
-from .utils import (bias_init_with_prob, constant_init,kaiming_init,
+from .utils import (bias_init_with_prob, constant_init, kaiming_init,
                     normal_init, uniform_init, xavier_init)
 
 __all__ = [
@@ -17,5 +18,5 @@ __all__ = [
     'build_padding_layer', 'build_upsample_layer',
     'is_norm', 'ACTIVATION_LAYERS',
     'CONV_LAYERS', 'NORM_LAYERS', 'PADDING_LAYERS', 'UPSAMPLE_LAYERS',
-    'PLUGIN_LAYERS','Scale'
+    'PLUGIN_LAYERS', 'Scale', 'NonLocal1d', 'NonLocal2d', 'NonLocal3d'
 ]
--- mmdet/cv_core/cnn/bricks/__init__.py
+++ mmdet/cv_core/cnn/bricks/__init__.py
@@ -7,6 +7,7 @@ from .registry import (ACTIVATION_LAYERS, CONV_LAYERS, NORM_LAYERS,
                        PADDING_LAYERS, PLUGIN_LAYERS, UPSAMPLE_LAYERS)
 from .upsample import build_upsample_layer
 from .scale import Scale
+from .non_local import NonLocal1d, NonLocal2d, NonLocal3d
 from .conv2d_adaptive_padding import Conv2dAdaptivePadding
 from .wrappers import Conv2d, ConvTranspose2d, Linear, MaxPool2d
 
@@ -15,5 +16,5 @@ __all__ = [
     'build_norm_layer', 'build_padding_layer', 'build_upsample_layer', 'is_norm',
     'ACTIVATION_LAYERS', 'CONV_LAYERS', 'NORM_LAYERS', 'PADDING_LAYERS',
     'UPSAMPLE_LAYERS', 'PLUGIN_LAYERS', 'Scale', 'Conv2dAdaptivePadding', 'Conv2d', 'ConvTranspose2d',
-    'MaxPool2d'
+    'MaxPool2d', 'NonLocal1d', 'NonLocal2d', 'NonLocal3d'
 ]
