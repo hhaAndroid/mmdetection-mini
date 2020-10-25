ad3122f87e18c474af066f67d832fb69d916cc8f
hha <1286304229@qq.com>
2020-10-25 09:38:08 +0800

    新增libra rcnn代码

--- mmdet/models/necks/__init__.py
+++ mmdet/models/necks/__init__.py
@@ -1,7 +1,8 @@
 from .fpn import FPN
+from .bfp import BFP
 from .yolo_neck import YOLOV3Neck
 from .rr_darkneck import RRDarkNeck, RRYoloV4DarkNeck
 
 __all__ = [
-    'FPN', 'YOLOV3Neck', 'RRDarkNeck', 'RRYoloV4DarkNeck'
+    'FPN', 'BFP', 'YOLOV3Neck', 'RRDarkNeck', 'RRYoloV4DarkNeck'
 ]
