91880d88715d7ee930e57629f4a9af9f598a152b
PJLAB\huanghaian <1286304229@qq.com>
2021-11-10 14:01:41 +0800

    fix error

--- tools/slurm_train.sh
+++ tools/slurm_train.sh
@@ -16,7 +16,7 @@ PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 srun -p ${PARTITION} \
     --job-name=${JOB_NAME} \
     --gres=gpu:${GPUS_PER_NODE} \
-    --ntasks=${GPUS} \
+    --ntasks=1 \
     --ntasks-per-node=${GPUS_PER_NODE} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
