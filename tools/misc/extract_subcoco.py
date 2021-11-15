0a2ba64bdc60bb90973da86f74becf817d51e766
hha <1286304229@qq.com>
2021-11-15 21:08:35 +0800

    support yolov5

--- tools/misc/extract_subcoco.py
+++ tools/misc/extract_subcoco.py
@@ -75,6 +75,71 @@ def _make_dir(output_dir):
     mmcv.mkdir_or_exist(osp.join(output_dir, 'stuffthingmaps/val2017'))
 
 
+import json
+import shutil
+from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
+
+
+def make_dirs(dir='new_dir/'):
+    # Create folders
+    dir = Path(dir)
+    for p in dir, dir / 'labels', dir / 'images':
+        p.mkdir(parents=True, exist_ok=True)  # make dir
+    return dir
+
+
+def coco91_to_coco80_class():  # converts 80-index (val2014) to 91-index (paper)
+    # https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/
+    # a = np.loadtxt('data/coco.names', dtype='str', delimiter='\n')
+    # b = np.loadtxt('data/coco_paper.names', dtype='str', delimiter='\n')
+    # x1 = [list(a[i] == b).index(True) + 1 for i in range(80)]  # darknet to coco
+    # x2 = [list(b[i] == a).index(True) if any(b[i] == a) else None for i in range(91)]  # coco to darknet
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, None, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, None, 24, 25, None,
+         None, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, None, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+         51, 52, 53, 54, 55, 56, 57, 58, 59, None, 60, None, None, 61, None, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,
+         None, 73, 74, 75, 76, 77, 78, 79, None]
+    return x
+
+
+def convert_coco_json(json_file, out,use_segments=False, cls91to80=True):
+    coco80 = coco91_to_coco80_class()
+
+    with open(json_file) as f:
+        data = json.load(f)
+
+    # Create image dict
+    images = {'%g' % x['id']: x for x in data['images']}
+
+    # Write labels file
+    for x in tqdm(data['annotations'], desc=f'Annotations {json_file}'):
+        if x['iscrowd']:
+            continue
+
+        img = images['%g' % x['image_id']]
+        h, w, f = img['height'], img['width'], img['file_name']
+
+        # The COCO box format is [top left x, top left y, width, height]
+        box = np.array(x['bbox'], dtype=np.float64)
+        box[:2] += box[2:] / 2  # xy top-left corner to center
+        box[[0, 2]] /= w  # normalize x
+        box[[1, 3]] /= h  # normalize y
+
+        # Segments
+        if use_segments:
+                segments = [j for i in x['segmentation'] for j in i]  # all segments concatenated
+                s = (np.array(segments).reshape(-1, 2) / np.array([w, h])).reshape(-1).tolist()
+
+        # Write
+        if box[2] > 0 and box[3] > 0:  # if w > 0 and h > 0
+                cls = coco80[x['category_id'] - 1] if cls91to80 else x['category_id'] - 1  # class
+                line = cls, *(s if use_segments else box)  # cls, box or segments
+                with open((Path(out) / f).with_suffix('.txt'), 'a') as file:
+                    file.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+
 def main():
     args = parse_args()
     assert args.output_dir != args.root
@@ -82,6 +147,9 @@ def main():
     _make_dir(args.output_dir)
     _process_data(args, 'train')
     _process_data(args, 'val')
+
+    convert_coco_json(f'{args.output_dir}/annotations/instances_train2017.json',f'{args.output_dir}/train2017')
+    convert_coco_json(f'{args.output_dir}/annotations/instances_val2017.json',f'{args.output_dir}/val2017')
 
 
 if __name__ == '__main__':
