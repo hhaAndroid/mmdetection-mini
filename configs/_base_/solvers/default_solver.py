506362bb52a35dbefdc054527334e71e61cf14e2
PJLAB\huanghaian <1286304229@qq.com>
2021-11-16 09:01:59 +0800

    fix lr

--- configs/_base_/solvers/default_solver.py
+++ configs/_base_/solvers/default_solver.py
@@ -1,6 +1,6 @@
 optimizer = dict(
     type='build_default_optimizer',
-    optimizer_cfg=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001),
+    optimizer_cfg=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001),
     paramwise_cfg=None,
 )
 
